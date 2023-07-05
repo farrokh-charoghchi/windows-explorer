d33ec7e010aefc4df8e9fb3b97af95038b651b3b
farrokh <farrokh.charoghchi@gmail.com>
2023-07-05 14:37:41 +0330

    Running server by Auto Host IP detection

--- windows-explorer/windows-explorer/Program.cs
+++ windows-explorer/windows-explorer/Program.cs
@@ -16,11 +16,33 @@ namespace windows_explorer
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+
+                    var hostName = System.Net.Dns.GetHostName();
+                    var ips = System.Net.Dns.GetHostAddresses(hostName).Where(ip => ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+                    var ipArr = ips.Select(ip => ip.ToString()).ToList();
+
+
+                    var CustomHost = config.GetSection("CustomHost").Value;
+                    if (!string.IsNullOrEmpty(CustomHost))
+                    {
+                        ipArr.Add(CustomHost);
+                    }
+                    
+                    var CustomPort = config.GetSection("CustomPort").Value;
+                    var urls = string.Join(";", ipArr.Select(ip => $@"http://{ip}:{CustomPort}").ToArray());
+
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls(urls);
                 });
+        }
     }
 }
