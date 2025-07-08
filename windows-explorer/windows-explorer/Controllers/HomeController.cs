e25b0886ce48e2aae9e16dba565bdf0cdf2930eb
Farrokh <farrokh.charoghchi@gmail.com>
2025-07-08 05:03:21 +0430

    sample-data-for-file-manager

--- windows-explorer/windows-explorer/Controllers/HomeController.cs
+++ windows-explorer/windows-explorer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Diagnostics;
 using windows_explorer.Models;
@@ -16,7 +17,44 @@ namespace windows_explorer.Controllers
 
         public IActionResult Index()
         {
+            ViewData["ver"] = Request.QueryString.Value?.Contains("ver") ?? false ? "1" : "0";
             return View();
+        }
+
+        [Route("/home/file-manager-file-system")]
+        [AcceptVerbs("get","post")]
+        public IActionResult fileManagerFileSystem()
+        {
+            List<object> res =
+            [
+                new
+                {
+                    key = "MyFile.jpg",
+                    name = "MyFile.jpg",
+                    dateModified = "2025-07-02T20:22:59.6911492Z",
+                    isDirectory = false,
+                    size = 1024,
+                    hasSubDirectories = false
+                    //thumbnail = "/fileicon?type=jpg",
+                },
+                new
+                {
+                    key = "MyFile2.mp4",
+                    name = "MyFile2.mp4",
+                    dateModified = "2025-07-02T20:22:59.6911492Z",
+                    isDirectory = false,
+                    size = 1024123456,
+                    hasSubDirectories = false,
+                    //thumbnail = "https://www.citypng.com/public/uploads/preview/yellow-computer-folder-icon-download-png-7017516950339154cbr0m5roh.png",
+                },
+                //{"key":"cldr-data","name":"cldr-data","dateModified":"2025-07-02T16:01:56.4536709Z","isDirectory":true,"size":0,"hasSubDirectories":true}
+            ];
+            return Json(new{
+                success = true,
+                errorCode = 0,
+                errorText = "",
+                result = res
+            });
         }
 
         public IActionResult Privacy()
